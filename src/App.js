17abdcc08a934bdb48da3a164dac1d1fddf1f844
Shuky <vojinsundovic@gmail.com>
2022-05-23 13:53:59 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import './App.css';
+import ValueButton from "./components/ValueButton";
+
+
+function App() {
+    const [result, setResult] = useState("");
+
+    const klikDugme = (e) => {
+        setResult(result.concat(e.target.name));
+    }
+
+    const backspace = () => {
+        setResult("");
+    }
+
+    const racunanjeVrednosti = () => {
+        try {
+            setResult(eval(result).toString());
+        }
+        catch (greska) {
+            setResult("Greska pri unosu");
+            setTimeout(backspace, 2000);
+        }
+
+    }
+
+    return (
+        <div className="App">
+            <div className="calculator">
+                <div className="calc-out">
+                    <input type="text" value={result} />
+                </div>
+                <div className="calc-in">
+                    <div className="row">
+                        <ValueButton name="7" value="7" fun={klikDugme} />
+                        <ValueButton name="8" value="8" fun={klikDugme} />
+                        <ValueButton name="9" value="9" fun={klikDugme} />
+                        <ValueButton name="+" value="+" fun={klikDugme} />
+                    </div>
+                    <div className="row">
+                        <ValueButton name="4" value="4" fun={klikDugme} />
+                        <ValueButton name="5" value="5" fun={klikDugme} />
+                        <ValueButton name="6" value="6" fun={klikDugme} />
+                        <ValueButton name="-" value="-" fun={klikDugme} />
+                    </div>
+                    <div className="row">
+                        <ValueButton name="1" value="1" fun={klikDugme} />
+                        <ValueButton name="2" value="2" fun={klikDugme} />
+                        <ValueButton name="3" value="3" fun={klikDugme} />
+                        <ValueButton name="*" value="&times;" fun={klikDugme} />
+                    </div>
+                    <div className="row" id="pos">
+                        <ValueButton name="0" value="0" fun={klikDugme} />
+                        <ValueButton id="sirok" name="del" value="Del" fun={backspace} />
+                        <ValueButton name="=" value="=" fun={racunanjeVrednosti} />
+                        <ValueButton name="/" value="&divide;" fun={klikDugme} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/ValueButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './valueButton.css';
+
+export default function ValueButton({name, value, fun, id}) {
+  return (
+    <button id={id} name={name} onClick={fun}>{value}</button>
+  )
+}
